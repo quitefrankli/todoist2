55a1daa57b3b6e0805c9858be5d96756a71069c7
Frank <quitefrankli@hotmail.com>
2023-12-02 23:51:26 +1100

    add delete button

--- main.py
+++ main.py
@@ -20,22 +20,25 @@ class Goal:
 class App(Frame):
     WIDTH = 500
     HEIGHT = 500
-    GOALS_CANVAS_WIDTH = WIDTH/2
-    GOAL_ENTRY_HEIGHT = 15
+    # GOALS_CANVAS_WIDTH = WIDTH-10
+    GOALS_CANVAS_WIDTH = WIDTH * 2/3
+    GOAL_ENTRY_HEIGHT = 25
     MAX_GOALS = 30
     SAVE_FILE = f"{Path.home()}/.todoist2_goals.yaml"
 
     def __init__(self, master=None):
         super().__init__(master)
         self.master.title("Todoist2")
-        self.master.geometry(f"{self.WIDTH}x{self.HEIGHT}")
-        self.master.eval('tk::PlaceWindow . center')
-        self.pack()
-
+        X = int((self.master.winfo_screenwidth() - self.WIDTH)/2)
+        Y = int((self.master.winfo_screenheight() - self.HEIGHT)/2)
+        self.master.geometry(f"{self.WIDTH}x{self.HEIGHT}+{X}+{Y}")
         Label(self, text="Goals").pack()
 
-        self.scrollbar = Scrollbar(self)
-        self.canvas = Canvas(self, yscrollcommand=self.scrollbar.set)
+        self.canvas_frame = Frame(self)
+
+
+        self.scrollbar = Scrollbar(self.canvas_frame)
+        self.canvas = Canvas(self.canvas_frame, yscrollcommand=self.scrollbar.set)
 
         self.goals = self.load_goals()
         self.scrollbar.configure(command=self.canvas.yview)
@@ -48,13 +51,16 @@ class App(Frame):
         self.goal_entry_windows = []
         self.refresh_goals_canvas()
             
-        self.scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
         self.canvas.pack(side=tkinter.LEFT)
+        self.scrollbar.pack(side=tkinter.LEFT, fill=tkinter.Y)
         self.canvas.config(width=self.GOALS_CANVAS_WIDTH, height=300)
+
+        self.canvas_frame.pack()
 
         Button(self,
                text="New Goal",
                command=self.spawn_new_goal_creation_window).pack(side=tkinter.BOTTOM)
+        self.pack()
 
     def refresh_goals_canvas(self) -> None:
         # first cleanup the canvas if there are existing goal windows
@@ -62,21 +68,31 @@ class App(Frame):
 
         for i in range(len(self.goals)):
             goal = self.goals[i]
-            self.goal_entry_windows.append(
-                self.canvas.create_window(0, 
-                                          i*self.GOAL_ENTRY_HEIGHT, 
-                                          window=Label(self.canvas, text=goal.name), 
-                                          anchor=tkinter.NW))
-            CHECK_BOX_RIGHT_OFFSET = self.GOAL_ENTRY_HEIGHT + 1
-            self.goal_entry_windows.append(
-                self.canvas.create_window(self.GOALS_CANVAS_WIDTH-CHECK_BOX_RIGHT_OFFSET,
-                                          i*self.GOAL_ENTRY_HEIGHT,
-                                          window=Checkbutton(self.canvas,
-                                                             variable=goal.get_tkinter_var(),
-                                                             onvalue=True,
-                                                             offvalue=False,
-                                                             command=lambda: self.save_goals()),
-                                          anchor=tkinter.NW))
+            self.canvas.create_window(0, 
+                                      i*self.GOAL_ENTRY_HEIGHT, 
+                                      anchor=tkinter.NW,
+                                      window=Label(self.canvas, text=goal.name))
+            DELETE_BUTTON_OFFSET = 40
+            CHECK_BOX_RIGHT_OFFSET = DELETE_BUTTON_OFFSET + self.GOAL_ENTRY_HEIGHT
+            self.canvas.create_window(self.GOALS_CANVAS_WIDTH-CHECK_BOX_RIGHT_OFFSET,
+                                      i*self.GOAL_ENTRY_HEIGHT,
+                                      anchor=tkinter.NW,
+                                      window=Checkbutton(self.canvas,
+                                                         variable=goal.get_tkinter_var(),
+                                                         onvalue=True,
+                                                         offvalue=False,
+                                                         command=lambda: self.save_goals()))
+            self.canvas.create_window(self.GOALS_CANVAS_WIDTH-DELETE_BUTTON_OFFSET,
+                                      i*self.GOAL_ENTRY_HEIGHT,
+                                      anchor=tkinter.NW,
+                                      window=Button(self.canvas,
+                                                    text='delete',
+                                                    command=lambda idx=i: self.delete_goal(idx)))
+        
+    def delete_goal(self, idx: int) -> None:
+        self.goals.pop(idx)
+        self.save_goals()
+        self.refresh_goals_canvas()
 
     def load_goals(self) -> List[Goal]:
         try:
