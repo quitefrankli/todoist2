04523f8acbd21a93c15ea7190ae0f662e30cf540
Frank <quitefrankli@hotmail.com>
2023-12-06 14:01:39 +1100

    add border and font

--- desktop_app.py
+++ desktop_app.py
@@ -3,7 +3,7 @@ import yaml
 import datetime
 
 from pathlib import Path
-from datetime import datetime
+from datetime import datetime, timedelta
 from tkinter import ttk, Frame, Label, Button, Entry, Scrollbar, Canvas, Checkbutton
 from typing import *
 from goal import Goal
@@ -14,9 +14,11 @@ class DesktopApp(Frame):
     WIDTH = 500
     HEIGHT = 500
     # GOALS_CANVAS_WIDTH = WIDTH-10
-    GOALS_CANVAS_WIDTH = WIDTH * 2/3
+    GOALS_CANVAS_WIDTH = WIDTH * 4/5
+    GOALS_CANVAS_HEIGHT = HEIGHT * 3/4
     GOAL_ENTRY_HEIGHT = 25
     MAX_GOALS = 30
+    GOAL_ENTRY_FONT = 'Helvetica 9'
 
     def __init__(self):
         super().__init__(None)
@@ -26,7 +28,7 @@ class DesktopApp(Frame):
         X = int((self.master.winfo_screenwidth() - self.WIDTH)/2)
         Y = int((self.master.winfo_screenheight() - self.HEIGHT)/2)
         self.master.geometry(f"{self.WIDTH}x{self.HEIGHT}+{X}+{Y}")
-        Label(self, text="Goals").pack()
+        Label(self, text="Goals", font='Helvetica 18 bold').pack()
 
         self.canvas_frame = Frame(self)
 
@@ -38,13 +40,14 @@ class DesktopApp(Frame):
         self.canvas.configure(scrollregion=(0, 
                                             0, 
                                             self.GOALS_CANVAS_WIDTH, 
-                                            self.MAX_GOALS * self.GOAL_ENTRY_HEIGHT))
+                                            self.MAX_GOALS * self.GOAL_ENTRY_HEIGHT),
+                              highlightthickness=2, highlightbackground="black")
 
         self.refresh_goals_canvas()
             
         self.canvas.pack(side=tkinter.LEFT)
         self.scrollbar.pack(side=tkinter.LEFT, fill=tkinter.Y)
-        self.canvas.config(width=self.GOALS_CANVAS_WIDTH, height=300)
+        self.canvas.config(width=self.GOALS_CANVAS_WIDTH, height=self.GOALS_CANVAS_HEIGHT)
 
         self.canvas_frame.pack()
         self.pack()
@@ -64,25 +67,41 @@ class DesktopApp(Frame):
         self.canvas.delete(tkinter.ALL)
 
         goals = self.client.fetch_goals()
+        offset = 0
+        x_padding = 2
+        y_padding = 2
+        last_date_label = datetime(year=2000, month=1, day=1).date()
         for i in range(len(goals)):
+            def get_y_val() -> int:
+                return (i + offset) * self.GOAL_ENTRY_HEIGHT
             goal = goals[i]
+            goal_date = goal.metadata.creation_date.date()
+            if last_date_label != goal_date:
+                last_date_label = goal_date
+                self.canvas.create_window(self.GOALS_CANVAS_WIDTH/2,
+                                          get_y_val(),
+                                          anchor=tkinter.N,
+                                          window=Label(self.canvas, 
+                                                       text=last_date_label.strftime("%d/%m/%Y"),
+                                                       font=f'{self.GOAL_ENTRY_FONT} bold'))
+                offset += 1
             self.canvas.create_window(0, 
-                                      i*self.GOAL_ENTRY_HEIGHT, 
+                                      get_y_val(), 
                                       anchor=tkinter.NW,
-                                      window=Label(self.canvas, text=goal.name))
+                                      window=Label(self.canvas, text=goal.name, font=self.GOAL_ENTRY_FONT))
             DELETE_BUTTON_OFFSET = 40
             CHECK_BOX_RIGHT_OFFSET = DELETE_BUTTON_OFFSET + self.GOAL_ENTRY_HEIGHT
             goal._tkinter_state = tkinter.BooleanVar(value=goal.state)
-            self.canvas.create_window(self.GOALS_CANVAS_WIDTH-CHECK_BOX_RIGHT_OFFSET,
-                                      i*self.GOAL_ENTRY_HEIGHT,
+            self.canvas.create_window(self.GOALS_CANVAS_WIDTH-CHECK_BOX_RIGHT_OFFSET-y_padding,
+                                      get_y_val(),
                                       anchor=tkinter.NW,
                                       window=Checkbutton(self.canvas,
                                                          variable=goal._tkinter_state,
                                                          onvalue=True,
                                                          offvalue=False,
                                                          command=lambda idx=i: self.client.toggle_goal_state(idx)))
-            self.canvas.create_window(self.GOALS_CANVAS_WIDTH-DELETE_BUTTON_OFFSET,
-                                      i*self.GOAL_ENTRY_HEIGHT,
+            self.canvas.create_window(self.GOALS_CANVAS_WIDTH-DELETE_BUTTON_OFFSET-y_padding,
+                                      get_y_val(),
                                       anchor=tkinter.NW,
                                       window=Button(self.canvas,
                                                     text='delete',
