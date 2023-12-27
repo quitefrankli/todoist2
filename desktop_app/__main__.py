e05e8718ecfc5cad4ade2e3b538168219561aa8e
Frank <quitefrankli@hotmail.com>
2023-12-28 01:15:31 +1100

    fix bug where pythonw does not launch properly due to click on windows as shortcut

--- desktop_app/__main__.py
+++ desktop_app/__main__.py
@@ -6,7 +6,6 @@ from .desktop_app import DesktopApp
 @click.command()
 @click.option("--debug/--no-debug", default=False)
 def main(debug: bool):
-	click.echo("Running desktop app")
 	app = DesktopApp(debug=debug)
 	app.run()
 
